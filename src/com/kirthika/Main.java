0349d9c32b880e46dc06af6cc64ddc49e03bc89d
Kirthika S <kirthika05@gmail.com>
2017-01-27 19:39:47 -0600

    Initial commit

---
+++ src/com/kirthika/Main.java
@@ -0,0 +1,40 @@
+package com.kirthika;
+
+import java.util.*;
+
+class Printer <T> {
+
+    public void printArray(T[] a){
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
